2b694ed65964bdc3ee4c883c1a0f29a9cbf3a3e3
Caio Martins <dev.caiomartins@gmail.com>
2020-10-19 19:09:28 +0100

    finished adding, deleting, and editing contacts

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -3,5 +3,8 @@ from phonebook import views
 
 urlpatterns = [
     path('', views.home, name='nome'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('contacts/delete/<int:id>/', views.delete, name='delete'),
+    path('contacts/create', views.create, name='create'),
+    path('contacts/edit/<int:id>/', views.edit, name='edit')
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        return redirect('/contacts')
+    else:
+        return render(request, 'home.html', {})
+
 
 def contacts(request):
     if request.user.is_authenticated:
@@ -14,4 +18,33 @@ def contacts(request):
         return render(request, 'contacts.html', context)
     else: 
         return render(request, 'error.html', {})
+
+def delete(request, id):
+    if request.method == 'POST':
+        Contact.objects.get(id=id).delete()
+        return redirect('/contacts')
+
+def create(request):
+    if request.method == 'POST':
+        contact = Contact(
+            first_name = request.POST['first_name'],
+            second_name = request.POST['second_name'],
+            email = request.POST['email'],
+            phone_number = request.POST['phone_number'],
+            user = request.user
+        )
+        contact.save()
+        return redirect('/contacts')
+
+def edit(request, id):
+    if request.method == 'POST':
+        c = Contact.objects.get(id=id)
+        c.first_name = request.POST['first_name']
+        c.second_name = request.POST['second_name']
+        c.email = request.POST['email']
+        c.phone_number = request.POST['phone_number']
+        c.save()
+        return redirect('/contacts')
+        
+
 
